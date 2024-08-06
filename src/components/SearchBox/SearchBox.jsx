e045bb4ee44f920a29af4654f49f83c46d05e945
InD1909 <ilyado1909@gmail.com>
2024-08-06 21:15:50 +0300

    changes

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,6 +1,15 @@
+import { useDispatch, useSelector } from "react-redux";
+import { changeFilter, selectNameFilter } from "../../redux/filtersSlice";
 import s from "./SearchBox.module.css";
 
-const SearchBox = ({ filter, onFilter }) => {
+const SearchBox = () => {
+  const dispatch = useDispatch();
+  const filter = useSelector(selectNameFilter);
+
+  const handleChange = (e) => {
+    dispatch(changeFilter(e.target.value));
+  };
+
   return (
     <div>
       <span className={s.wrapper}>
@@ -8,7 +17,7 @@ const SearchBox = ({ filter, onFilter }) => {
         <input
           type="text"
           value={filter}
-          onChange={(e) => onFilter(e.target.value)}
+          onChange={handleChange}
           className={s.searchInput}
         />
       </span>
